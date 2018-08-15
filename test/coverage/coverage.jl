cdb6b21be201542edbd22341f23e3e458597f7e2
Morten Piibeleht <morten.piibeleht@gmail.com>
2018-08-15 17:38:52 +1200

    Use a Pkg environment for coverage (#1)
    * Use a Project.toml for coverage
    
    * Instantiate test/coverage
    
    * Fix directory in coverage.jl

--- test/coverage/coverage.jl
+++ test/coverage/coverage.jl
@@ -2,9 +2,8 @@
 get(ENV, "TRAVIS_OS_NAME", "")       == "linux"   || exit()
 get(ENV, "TRAVIS_JULIA_VERSION", "") == "nightly" || exit()
 
-Pkg.add("Coverage")
 using Coverage
 
-cd(joinpath(dirname(@__FILE__), "..")) do
+cd(joinpath(dirname(@__FILE__), "..", "..")) do
     Codecov.submit(Codecov.process_folder())
 end
