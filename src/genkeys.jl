03cc8509d2376e6fb0f5791cd9a15153a2c9aacd
Fredrik Ekre <ekrefredrik@gmail.com>
2019-10-08 14:31:25 +0200

    Generalize genkeys.

--- src/genkeys.jl
+++ src/genkeys.jl
@@ -1,23 +1,22 @@
-"""
-Package functions for interacting with Travis.
-
-$(EXPORTS)
-"""
+# Deprecated module
+export Travis
 module Travis
+    import ..DocumenterTools
+    export genkeys
+    function genkeys(args...; kwargs...)
+        Base.depwarn("Travis.genkeys is deprecated, use DocumenterTools.genkeys instead", :genkeys)
+        return DocumenterTools.genkeys(args...; kwargs...)
+    end
+end # module
 
-using DocStringExtensions
 using Base64
 import LibGit2: GITHUB_REGEX
-using ..DocumenterTools: package_devpath
-
-export genkeys
-
 
 """
-    genkeys(; user="\$USER", repo="\$REPO")
+    DocumenterTools.genkeys(; user="\$USER", repo="\$REPO")
 
 Generates the SSH keys necessary for the automatic deployment of documentation with
-Documenter from Travis to GitHub Pages.
+Documenter from a builder to GitHub Pages.
 
 By default the links in the instructions need to be modified to correspond to actual URLs.
 The optional `user` and `repo` keyword arguments can be specified so that the URLs in the
@@ -36,22 +35,22 @@ installed:
 ```julia-repl
 julia> using DocumenterTools
 
-julia> Travis.genkeys()
+julia> DocumenterTools.genkeys()
 [ Info: add the public key below to https://github.com/\$USER/\$REPO/settings/keys with read/write access:
 
 ssh-rsa AAAAB3NzaC2yc2EAAAaDAQABAAABAQDrNsUZYBWJtXYUk21wxZbX3KxcH8EqzR3ZdTna0Wgk...jNmUiGEMKrr0aqQMZEL2BG7 username@hostname
 
-[ Info: add a secure environment variable named 'DOCUMENTER_KEY' to https://travis-ci.com/\$USER/\$REPO/settings with value:
+[ Info: add a secure environment variable named 'DOCUMENTER_KEY' to https://travis-ci.com/\$USER/\$REPO/settings (if you deploy using Travis CI) or https://github.com/\$USER/\$REPO/settings/secrets (if you deploy using GitHub Actions) with value:
 
 LS0tLS1CRUdJTiBSU0EgUFJJVkFURSBLRVktLS0tLQpNSUlFb3dJQkFBS0NBUUVBNnpiRkdXQVZpYlIy...QkVBRWFjY3BxaW9uNjFLaVdOcDU5T2YrUkdmCi0tLS0tRU5EIFJTQSBQUklWQVRFIEtFWS0tLS0tCg==
 
 
-julia> Travis.genkeys(user="JuliaDocs", repo="DocumenterTools.jl")
+julia> DocumenterTools.genkeys(user="JuliaDocs", repo="DocumenterTools.jl")
 [Info: add the public key below to https://github.com/JuliaDocs/DocumenterTools.jl/settings/keys with read/write access:
 
 ssh-rsa AAAAB3NzaC2yc2EAAAaDAQABAAABAQDrNsUZYBWJtXYUk21wxZbX3KxcH8EqzR3ZdTna0Wgk...jNmUiGEMKrr0aqQMZEL2BG7 username@hostname
 
-[ Info: add a secure environment variable named 'DOCUMENTER_KEY' to https://travis-ci.com/JuliaDocs/DocumenterTools.jl/settings with value:
+[ Info: add a secure environment variable named 'DOCUMENTER_KEY' to https://travis-ci.com/JuliaDocs/DocumenterTools.jl/settings (if you deploy using Travis CI) or https://github.com/JuliaDocs/DocumenterTools.jl/settings/secrets (if you deploy using GitHub Actions) with value:
 
 LS0tLS1CRUdJTiBSU0EgUFJJVkFURSBLRVktLS0tLQpNSUlFb3dJQkFBS0NBUUVBNnpiRkdXQVZpYlIy...QkVBRWFjY3BxaW9uNjFLaVdOcDU5T2YrUkdmCi0tLS0tRU5EIFJTQSBQUklWQVRFIEtFWS0tLS0tCg==
 ```
@@ -80,8 +79,11 @@ function genkeys(; user="\$USER", repo="\$REPO")
     # Base64 encode the private key and prompt user to add it to travis. The key is
     # *not* encoded for the sake of security, but instead to make it easier to
     # copy/paste it over to travis without having to worry about whitespace.
-    let url = "https://travis-ci.com/$user/$repo/settings"
-        @info("add a secure environment variable named 'DOCUMENTER_KEY' to $url with value:")
+    let travis_url = "https://travis-ci.com/$user/$repo/settings",
+        github_url = "https://github.com/$user/$repo/settings/secrets"
+        @info("add a secure environment variable named 'DOCUMENTER_KEY' to " *
+              "$(travis_url) (if you deploy using Travis CI) or " *
+              "$(github_url) (if you deploy using GitHub Actions) with value:")
         println("\n", base64encode(read(filename, String)), "\n")
         rm(filename)
     end
@@ -91,7 +93,7 @@ end
     genkeys(package::Module; remote="origin")
 
 Like the other method, this generates the SSH keys necessary for the automatic deployment of
-documentation with Documenter from Travis to GitHub Pages, but attempts to guess the package
+documentation with Documenter from a builder to GitHub Pages, but attempts to guess the package
 URLs from the Git remote.
 
 `package` needs to be the top level module of the package. The `remote` keyword argument can
@@ -113,12 +115,12 @@ This method requires the following command lines programs to be installed:
 ```julia-repl
 julia> using DocumenterTools
 
-julia> Travis.genkeys(DocumenterTools)
+julia> DocumenterTools.genkeys(DocumenterTools)
 [Info: add the public key below to https://github.com/JuliaDocs/DocumenterTools.jl/settings/keys with read/write access:
 
 ssh-rsa AAAAB3NzaC2yc2EAAAaDAQABAAABAQDrNsUZYBWJtXYUk21wxZbX3KxcH8EqzR3ZdTna0Wgk...jNmUiGEMKrr0aqQMZEL2BG7 username@hostname
 
-[ Info: add a secure environment variable named 'DOCUMENTER_KEY' to https://travis-ci.com/JuliaDocs/DocumenterTools.jl/settings with value:
+[ Info: add a secure environment variable named 'DOCUMENTER_KEY' to https://travis-ci.com/JuliaDocs/DocumenterTools.jl/settings (if you deploy using Travis CI) or https://github.com/JuliaDocs/DocumenterTools.jl/settings/secrets (if you deploy using GitHub Actions) with value:
 
 LS0tLS1CRUdJTiBSU0EgUFJJVkFURSBLRVktLS0tLQpNSUlFb3dJQkFBS0NBUUVBNnpiRkdXQVZpYlIy...QkVBRWFjY3BxaW9uNjFLaVdOcDU5T2YrUkdmCi0tLS0tRU5EIFJTQSBQUklWQVRFIEtFWS0tLS0tCg==
 ```
@@ -132,7 +134,7 @@ function genkeys(package::Module; remote="origin")
 
     # Are we in a git repo?
     user, repo = cd(path) do
-        success(`git status`) || error("Failed to run `git status` in $(path). 'Travis.genkeys' only works with Git repositories.")
+        success(`git status`) || error("Failed to run `git status` in $(path). 'DocumenterTools.genkeys' only works with Git repositories.")
 
         let r = readchomp(`git config --get remote.$remote.url`)
             m = match(GITHUB_REGEX, r)
@@ -144,5 +146,3 @@ function genkeys(package::Module; remote="origin")
     # Generate the ssh key pair.
     genkeys(; user=user, repo=repo)
 end
-
-end # module
