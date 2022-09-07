3c720948b2e3cbb7c94dc42216cec2d0de5e7f3f
Morten Piibeleht <morten.piibeleht@gmail.com>
2022-09-07 16:00:28 +1200

    Show ssh-keygen output and error if it fails
    For #64.

--- src/genkeys.jl
+++ src/genkeys.jl
@@ -58,7 +58,12 @@ function genkeys(; user="\$USER", repo="\$REPO")
     isfile("$(filename).pub") && error("temporary file '$(filename).pub' already exists in working directory")
 
     # Generate the ssh key pair.
-    success(`$(sshkeygen) -N "" -C Documenter -m PEM -f $filename`) || error("failed to generate a SSH key pair.")
+    try
+        run(`$(sshkeygen) -N "" -C Documenter -m PEM -f $filename`)
+    catch e
+        @error "failed to generate a SSH key pair." exception = (e, catch_backtrace())
+        rethrow(e)
+    end
 
     # Prompt user to add public key to github then remove the public key.
     let url = "https://github.com/$user/$repo/settings/keys"
