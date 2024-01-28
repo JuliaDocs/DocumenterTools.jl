5306737042cd5152685fd807417d590a1ead22f4
Morten Piibeleht <morten.piibeleht@juliahub.com>
2024-01-28 16:39:41 +1300

    Fix the issue URLs in CHANGELOG.md

--- docs/changelog.jl
+++ docs/changelog.jl
@@ -3,5 +3,5 @@ using Changelog
 Changelog.generate(
     Changelog.CommonMark(),
     joinpath(@__DIR__, "..", "CHANGELOG.md");
-    repo = "JuliaDocs/IOCapture.jl",
+    repo = "JuliaDocs/DocumenterTools.jl",
 )
